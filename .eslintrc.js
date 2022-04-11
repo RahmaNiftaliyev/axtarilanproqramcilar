83d4d8ffd9144b606a58802c22f0f73618d3e7f6
nothing <58683199+RahmaNiftaliyev@users.noreply.github.com>
2022-04-11 06:57:14 +0400

    eslint  config updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
+// This is required for the `@babel/plugin-transform-runtime` plugin
+
 export default {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -11,6 +20,47 @@ export default {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: [new SVGSwitchElement()],
-  rules: {},
+  plugins: [
+    new SVGSwitchElement(),
+    "react",
+    "react-hooks",
+    "jsx-a11y",
+    "import",
+    "prettier",
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/no-unescaped-entities": "off",
+    "react/no-unused-state": "off",
+    "react/no-unused-prop-types": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-curly-brace-presence": "off",
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".js", ".jsx"],
+      },
+    ],
+    "react/jsx-wrap-multilines": "off",
+    "react/jsx-indent": "off",
+    "react/jsx-indent-props": "off",
+    "react/jsx-closing-bracket-location": "off",
+    "react/jsx-indent-props": "off",
+    "react/jsx-tag-spacing": "off",
+    "react/jsx-indent-props": "off",
+    "react/jsx-indent": "off",
+    "react/jsx-closing-bracket-location": "off",
+    "react/jsx-wrap-multilines": "off",
+    "react/jsx-curly-brace-presence": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/no-unescaped-entities": "off",
+    "react/no-unused-state": "off",
+    "react/no-unused-prop-types": "off",
+    "react/display-name": "off",
+    "react/prop-types": "off",
+    "react/jsx-indent": "off",
+  },
 };
