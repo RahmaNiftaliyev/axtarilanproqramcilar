752f0310fdeebd9b5f76b02036dd2d6bb541f9ea
krakenboy <info@rahmanniftaliyev.com>
2022-04-13 23:02:47 +0400

    global-errors-fixed

--- src/features/tool/calendar/ReactCalendar.jsx
+++ src/features/tool/calendar/ReactCalendar.jsx
@@ -1,28 +1,10 @@
 import React from 'react';
-import InfiniteCalendar from 'react-infinite-calendar';
-import {
-  withDateSelection,
-  withKeyboardSupport,
-  Calendar,
-} from 'react-infinite-calendar';
-import 'react-infinite-calendar/styles.css'; // Make sure to import the default stylesheet
+
+
 const ReactCalendar = () => {
   return (
     <div>
-      <InfiniteCalendar
-        locale={{
-          weekStartsOn: 1,
-        }}
-        Component={withDateSelection(withKeyboardSupport(Calendar))}
-        selected={new Date()}
-        width={window.innerWidth <= 650 ? window.innerWidth : "100%"}
-        height={window.innerHeight}
-        rowHeight={70}
-        onSelect={function (date) {
-          alert('You selected: ' + (date + ' ddd, MMM Do YYYY'));
-        }}
-        className={`react-wanted-calendar`}
-      />
+      <h1>React Calendar</h1>
     </div>
   );
 };
--- src/features/tool/cesium/Cesium.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/react-in-jsx-scope */
-import { Viewer } from 'resium';
-
-const Cesium = () => {
-  return <Viewer full />;
-};
-
-export default Cesium;
