48e7ed83157362025b8bbeda0a369cc2a84d3934
krakenboy <info@rahmanniftaliyev.com>
2022-04-22 09:30:08 +0400

    logic layers for standart demo database builded

--- wantedclientside/src/features/root/Dashboard.js
+++ wantedclientside/src/features/root/Dashboard.js
@@ -11,6 +11,7 @@ import TopRanks from '../components/topranks/TopRanks';
 import OnlineCourses from '../components/onlinecourses/OnlineCourses';
 import TestView from './../test/manual/TestView';
 import { FaPastafarianism } from 'react-icons/fa';
+import Recovery from '../common/login/Recovery';
 
 const Dashboard = () => {
   const [freelancer, setFreelancer] = React.useState(false);
@@ -40,6 +41,7 @@ const Dashboard = () => {
           }
         />
         <Route path="/login" element={<Login />} />
+        <Route path="/recovery" element={<Recovery />} />
 
         {/*NAVIGATION LINKS EXPLORE TOP-RANKS ONLINE-COURSES */}
         <Route path="/explore" element={<Explore />} />
