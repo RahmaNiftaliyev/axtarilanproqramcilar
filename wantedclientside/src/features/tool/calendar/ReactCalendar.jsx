db777488d5e6bc9a50fc587c935b2a2e1ec9c83b
rahmaniftaliyev <info@rahmanniftaliyev.com>
2022-05-17 10:59:08 +0400

    calendar  section updated

--- wantedclientside/src/features/tool/calendar/ReactCalendar.jsx
+++ wantedclientside/src/features/tool/calendar/ReactCalendar.jsx
@@ -12,7 +12,10 @@ const ReactCalendar = () => {
   // !per 42 days of calendar
   const [oneView, setOnewView] = useState([]);
   const [impINdex, setImpIndex] = useState([]);
-
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
+  const [todaysDate, setTodaysDate] = useState(new Date().getDate());
+  const [trustedIndex, setTrustedIndex] = useState();
+  const [maxDays, setMaxDays] = useState([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]);
   const [weekDate, setWeekDate] = useState(['Sun', 'Mon', 'Tue', 'Wen', 'Thur', 'Fri', 'Sat']);
   const [strmonths, setStrmonths] = useState([
     'January',
@@ -33,20 +36,100 @@ const ReactCalendar = () => {
     return <th key={index}>{day}</th>;
   });
 
+  const rendered42Days = (paramsImpIndex) => {
+    return paramsImpIndex.map((day, index) => {
+      return (
+        <tr key={index}>
+          {day.map((daymin, index) => {
+            return (
+              <td
+                value={daymin}
+                key={index}
+                className={`${daymin === todaysDate ? `${styles.active_calendar}` : ''}`}
+              >
+                {daymin}
+              </td>
+            );
+          })}
+        </tr>
+      );
+    });
+  };
+
   // !lIGHT AND DARK MODE BEGINS HERE
   const handleDarkAndLightThem = (e) => {
     setToggle(!toggle);
   };
 
+  const nextMonthSetter = () => {
+    if (currentMonth === 11) {
+      setCurrentMonth(0);
+    } else {
+      setCurrentMonth(currentMonth + 1);
+      setTodaysDate((pre) => {
+        let maximumNumber = maxDays[currentMonth];
+        let copyData = pre;
+        if (maximumNumber - copyData > 7) {
+          return pre + 7;
+        } else {
+          let newData = maximumNumber - copyData;
+          let newMonthsDate = 7 - newData;
+          return newMonthsDate;
+        }
+      });
+    }
+  };
 
+  const previousMonthSetter = () => {
+    if (currentMonth === 0) {
+      setCurrentMonth(11);
+    } else {
+      setTodaysDate((pre) => {
+        setCurrentMonth(currentMonth - 1);
+        let maximumNumber = maxDays[currentMonth];
+        let copyData = pre;
+        if (copyData - 7 > 0) {
+          return pre - 7;
+        } else {
+          let newData = copyData - 7;
+          let newMonthsDate = maximumNumber - newData;
+          return newMonthsDate;
+        }
+      });
+    }
+  };
+
+  // !CALENDAR FUNCTION BEGINS HERE
+  const checkForLeapYear = (year) => {
+    if (year % 4 === 0 && year % 100 !== 0) {
+      return true;
+    } else if (year % 400 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const setNumberOfDays = (year, month) => {
+    let numberOfDays;
+    if (month === 1) {
+      numberOfDays = checkForLeapYear(year) ? 29 : 28;
+    } else {
+      numberOfDays = maxDays[month];
+    }
+    return numberOfDays;
+  };
 
   useEffect(() => {
-    let today = new Date().getMonth();
-    let maxDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let newArr = [];
-    let newArr2 = [];
+    setCurrentMonth(new Date().getMonth());
+    let max42ForLoop = [];
+    let All42ArraysInside = [];
     let todayCounter = 0;
-    let settedArr = [];
+    let trueIndexed42 = [];
+    let sevenArr = [];
+    let sevenTotalArr = [];
+
+    let previousYearsDays = [31, 30, 29, 28, 27, 26];
 
     setAllDays((prevoiusValue) => {
       let newA = [...prevoiusValue];
@@ -54,25 +137,52 @@ const ReactCalendar = () => {
       maxDays.forEach((month) => {
         for (let i = 1; i <= month; i++) {
           newA.push(i);
+          if (newA.length === 365) {
+            for (let i = 0; i < previousYearsDays.length; i++) {
+              newA.pop();
+              newA.unshift(previousYearsDays[i]);
+            }
+          }
         }
       });
 
       if (newA.length === 365) {
         for (let i = 0; i < newA.length; i++) {
-          newArr.push(newA[i]);
-          if (newArr.length === 42) {
-            newArr2.push(newArr);
-            if (newArr2.length == 8) {
-              setOnewView(newArr2);
+          max42ForLoop.push(newA[i]);
+          if (max42ForLoop.length === 42) {
+            All42ArraysInside.push(max42ForLoop);
+            if (All42ArraysInside.length == 8) {
+              setOnewView(All42ArraysInside);
+              for (let i = 0; i < maxDays.length; i++) {
+                if (i < currentMonth) {
+                  todayCounter += maxDays[i];
+                } else {
+                  todayCounter = todayCounter + todaysDate;
+                  todayCounter = Math.floor(todayCounter / 42);
+                  setTrustedIndex(todayCounter);
+                  trueIndexed42 = oneView[trustedIndex];
+                  for (let i = 0; i < trueIndexed42.length; i++) {
+                    sevenArr.push(trueIndexed42[i]);
+                    if (sevenArr.length === 7) {
+                      sevenTotalArr.push(sevenArr);
+                      if (i === 41) {
+                        setImpIndex(sevenTotalArr);
+                      }
+                      sevenArr = [];
+                    }
+                  }
+                  break;
+                }
+              }
             }
-            newArr = [];
+            max42ForLoop = [];
           }
         }
       }
 
       return newA;
     });
-  }, []);
+  }, [currentMonth, trustedIndex]);
 
   return (
     <div
@@ -83,36 +193,57 @@ const ReactCalendar = () => {
       <button className={`${styles.theme_button}`} onClick={handleDarkAndLightThem}>
         {!toggle ? <MdDarkMode /> : <MdLightMode />}
       </button>
-      <div className={styles.calendar}>
-        {/* !CALENDAR HEADER LEFT AND RIGHT SIDE */}
-        <div className={`${styles.calendar_header}`}>
-          <div className={styles.calendar_header_left}>
-            <img
-              src={left}
-              alt="arrow-left"
-              className={`cursor-pointer ${toggle ? '' : styles.calendar_image_dark}`}
-              width="55"
-            />
-            <h3 className={`${toggle ? '' : styles.calendar_text_dark}`}></h3>
-            <img
-              src={right}
-              alt="arrow-right"
-              className={`cursor-pointer ${toggle ? '' : styles.calendar_image_dark}`}
-              width="55"
-            />
-          </div>
-          <div className={styles.calendar_header_right}>
-            <h3 className={`${toggle ? '' : styles.calendar_text_dark}`}>today</h3>
+      <div className={`${styles.calendar_container}`}>
+        <div className={`${styles.controller_side}`}>
+          <div className={`${styles.controller_container}`}>
+            <h2>Control Panel</h2>
+            <div className={`${styles.control_meetings}`}>
+              <h3>Your meetings</h3>
+            </div>
+            <div className={`${styles.control_create}`}>
+              <h3>Create a meeting</h3>
+            </div>
           </div>
         </div>
-        {/* !CALENDAR BODY DAYS AND MONTH */}
-        <div className={`${styles.calendar_body}`}>
-          <table cellPadding="6" cellSpacing="6">
-            <thead>
-              <tr>{renderedWeekDays}</tr>
-            </thead>
-            <tbody>{}</tbody>
-          </table>
+        <div className={`${styles.calendar_badge}`}>
+          <div className={styles.calendar}>
+            {/* !CALENDAR HEADER LEFT AND RIGHT SIDE */}
+            <div className={`${styles.calendar_header}`}>
+              <div className={styles.calendar_header_left}>
+                <img
+                  src={left}
+                  alt="arrow-left"
+                  className={`cursor-pointer ${toggle ? '' : styles.calendar_image_dark}`}
+                  width="55"
+                  onClick={previousMonthSetter}
+                />
+                <h3 className={`${toggle ? '' : styles.calendar_text_dark}`}>
+                  {strmonths[currentMonth]}
+                </h3>
+                <img
+                  src={right}
+                  alt="arrow-right"
+                  className={`cursor-pointer ${toggle ? '' : styles.calendar_image_dark}`}
+                  width="55"
+                  onClick={nextMonthSetter}
+                />
+              </div>
+              <div className={styles.calendar_header_right}>
+                <h3 className={`${toggle ? '' : styles.calendar_text_dark}`}>{`${todaysDate}/${
+                  new Date().getMonth() + 1
+                }/${new Date().getFullYear()}`}</h3>
+              </div>
+            </div>
+            {/* !CALENDAR BODY DAYS AND MONTH */}
+            <div className={`${styles.calendar_body}`}>
+              <table cellPadding="6" cellSpacing="6">
+                <thead>
+                  <tr>{renderedWeekDays}</tr>
+                </thead>
+                <tbody>{rendered42Days(impINdex)}</tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </div>
